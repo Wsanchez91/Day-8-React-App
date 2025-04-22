18c06329ba3275016cb7b25077cd9f5c242dfc01
William Sanchez <williamsanchez.8055@gmail.com>
2025-04-22 16:15:58 -0400

    Lesson 2-3

--- day-8-feedback-app/src/App.jsx
+++ day-8-feedback-app/src/App.jsx
@@ -1,11 +1,30 @@
-function Greeting() {
-  return <h1>Hello, React World!</h1>;
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Simple Counter</h2>
+      <p>Count: {count}</p>
+      <button onClick={()=> setCount(count + 1)}>Add</button>
+    </div>
+  )
+}
+
+function Greeting(props) {
+  return (
+    <h1>
+      Hello, {props.name} {props.test}
+    </h1>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <Greeting />
+      <Greeting name="William" test="Beast" />
+      <Counter />
     </div>
   );
 }
